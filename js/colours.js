ee224f3d947dab65ba282c7aa7f9cb455c855158
arkon <ey.cheung@outlook.com>
2014-12-16 16:25:07 -0500

    Initial most visited pages work.

--- js/colours.js
+++ js/colours.js
@@ -1,3 +1,6 @@
+/**
+ * Calculates and displays the time and corresponding (hexadecimal) colour.
+ */
 (function doTime() {
   var d     = new Date();
   var hours = d.getHours();
@@ -21,3 +24,39 @@
 
   setTimeout(doTime, 1000);
 })();
+
+/**
+ * Handle the showing/hiding of the Most Visited Pages panel.
+ */
+var mostVisited       = document.getElementById('chrome-contents');
+var mostVisitedToggle = document.getElementById('chrome-contents-toggle');
+mostVisitedToggle.onclick = function() {
+  var visible = document.body.classList.contains('show-chrome-contents');
+
+  if (visible)
+    document.body.classList.remove('show-chrome-contents');
+  else
+    document.body.classList.add('show-chrome-contents');
+
+  this.innerHTML = visible ? 'Show most visited' : 'Hide most visited';
+};
+
+/**
+ * Given an array of URLs, build a DOM list of these URLs in the NTP.
+ */
+function buildVisitedList(mostVisitedURLs) {
+  var visitedList = document.getElementById('mostVisited');
+
+  for (var i = 0; i < mostVisitedURLs.length; i++) {
+    var li   = visitedList.appendChild(document.createElement('li'));
+    var a    = li.appendChild(document.createElement('a'));
+    var site = mostVisitedURLs[i];
+
+    a.style.backgroundImage = 'url(chrome://favicon/' + site.url + ')';
+    a.href                  = site.url;
+    a.title                 = site.url;
+    a.innerHTML             = site.title;
+  }
+}
+
+chrome.topSites.get(buildVisitedList);
