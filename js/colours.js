aa0ca4775b4791a3d285998807ea31ef7d2f5ab8
arkon <ey.cheung@outlook.com>
2014-12-16 23:52:38 -0500

    Recently closed sessions

--- js/colours.js
+++ js/colours.js
@@ -57,12 +57,17 @@ function togglePanel(id) {
   chrome.storage.sync.set({ 'ntp_panel_visible': id });
 }
 
+// Display panel depending on synced state
+getConfig('ntp_panel_visible', togglePanel);
+
 
 /**
- * Given an array of URLs, build a DOM list of these URLs in the NTP.
+ * Given an array of URLs, build a DOM list of these URLs.
  */
 function buildVisitedList(visitedURLs) {
   var visitedList = document.getElementById('visited');
+
+  // visitedURLs.slice(0, getConfig('number_top'))
 
   for (var i = 0; i < visitedURLs.length; i++) {
     var li   = visitedList.appendChild(document.createElement('li'));
@@ -79,41 +84,76 @@ function buildVisitedList(visitedURLs) {
 // Get most visited sites
 chrome.topSites.get(buildVisitedList);
 
-// Display panel depending on synced state
-chrome.storage.sync.get('ntp_panel_visible', function (result) {
-  togglePanel(result.ntp_panel_visible);
-});
+
+/**
+ * Given an array of recently closed sessions, build a DOM list of the pages.
+ */
+function buildClosedList(sessions) {
+  var closedList = document.getElementById('closed');
+
+  for (var i = 0; i < sessions.length; i++) {
+    var li      = closedList.appendChild(document.createElement('li'));
+    var a       = li.appendChild(document.createElement('a'));
+    var session = sessions[i];
+
+    if (session.window && session.window.tabs.length === 1)
+      session.tab = session.window.tabs[0];
+
+    a.style.backgroundImage = session.tab ? 'url(chrome://favicon/' + session.tab.url + ')' : null;
+    a.href  = session.tab ? session.tab.url : null;
+    a.title = session.tab ? session.tab.title : session.window.tabs.length + ' Tabs';
+    a.innerHTML = session.tab ? session.tab.title : session.window.tabs.length + ' Tabs';
+  }
+}
+
+// Get recently closed sessions
+chrome.sessions.getRecentlyClosed(buildClosedList);
 
 
 /**
- * Given an array of apps, build a DOM list of
+ * Given an array of apps, build a DOM list of the apps.
  */
 function buildAppsList(list) {
   var appsList = document.getElementById('apps');
 
+  // Only get active apps (no extensions)
+  list = list.filter(function(a) {
+    return a.enabled && a.type !== 'extension' && a.type !== 'theme' && a.isApp
+  });
+
+  // Sort them alphabetically
+  list.sort(function (a, b) {
+    if (a.name < b.name) { return -1; }
+    else if (a.name > b.name) { return 1; }
+    else                      { return 0; }
+  });
+
   for (var i in list) {
-    // Only get active apps (no extensions)
+    var li = appsList.appendChild(document.createElement('li'));
+    var a  = li.appendChild(document.createElement('a'));
     var extInf = list[i];
-    if (extInf.isApp && extInf.enabled) {
-      var li = appsList.appendChild(document.createElement('li'));
-      var a  = li.appendChild(document.createElement('a'));
 
-      li.addEventListener('click', (function(ext) {
-        return function() {
-          chrome.management.launchApp(ext.id);
-        };
-      })(extInf));
+    li.addEventListener('click', (function(ext) {
+      return function() {
+        chrome.management.launchApp(ext.id);
+      };
+    })(extInf));
 
-      var img = a.appendChild(new Image());
-      img.src = find128Image(extInf.icons);
+    var img = a.appendChild(new Image());
+    img.src = find128Image(extInf.icons);
 
-      var name = a.appendChild(document.createElement('div'));
-      name.className = 'app-name';
-      name.innerHTML = extInf.name;
-    }
+    var name = a.appendChild(document.createElement('div'));
+    name.className = 'app-name';
+    name.innerHTML = extInf.name;
   }
+
+  var store       = appsList.appendChild(document.createElement('a'));
+  store.id        = 'store-link';
+  store.href      = 'https://chrome.google.com/webstore';
+  store.innerHTML = 'Chrome Web Store';
 }
 
+// Finds an 128px x 128px icon for an app
 function find128Image(icons) {
   for (var icon in icons) {
     if (icons[icon].size == '128') {
@@ -126,3 +166,13 @@ function find128Image(icons) {
 
 // Get apps
 chrome.management.getAll(buildAppsList);
+
+
+/**
+ * Storage helpers
+ */
+function getConfig(key, callback) {
+  chrome.storage.sync.get(key, function (result) {
+    callback(result[key]);
+  });
+}
