931764816c70527d7ecf59fb2277f21b26cb56cd
Eugene Cheung <ey.cheung@outlook.com>
2015-04-30 12:11:22 -0400

    Method for img to base64

--- js/core.js
+++ js/core.js
@@ -92,3 +92,27 @@ function getJSON (url, successHandler, errorHandler) {
 
   xhr.send();
 };
+
+
+/**
+ * Convert an image to a base64 url.
+ * Code from http://stackoverflow.com/a/20285053/4421500
+ * @param  {String}   url
+ * @param  {Function} callback
+ * @param  {String}   [outputFormat=image/png]
+ */
+function convertImgToBase64URL (url, callback, outputFormat) {
+  var img = new Image();
+  img.crossOrigin = 'Anonymous';
+  img.onload = function () {
+    var canvas = document.createElement('CANVAS'),
+    ctx = canvas.getContext('2d'), dataURL;
+    canvas.height = img.height;
+    canvas.width = img.width;
+    ctx.drawImage(img, 0, 0);
+    dataURL = canvas.toDataURL(outputFormat);
+    callback(dataURL);
+    canvas = null;
+  };
+  img.src = url;
+}
