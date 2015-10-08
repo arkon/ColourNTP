1d67e611bd739922e5b707f3cb69e32483ecdd14
Eugene Cheung <ey.cheung@outlook.com>
2015-10-08 19:45:00 -0400

    Add Chrome helpers

---
+++ app/scripts/modules/chrome.js
@@ -0,0 +1,116 @@
+class Chrome {
+
+    getTopSites (max) {
+        chrome.topSites.get(function (visitedURLs) {
+            // Consider the user's set maximum (default 10)
+            visitedURLs = visitedURLs.slice(0, Number(max) || 10);
+
+            var items = [];
+
+            for (var i in visitedURLs) {
+                items.push({
+                    title : site.title,
+                    url   : site.url,
+                    img   : `url(chrome://favicon/${site.url})`
+                });
+            }
+
+            return items;
+        });
+    }
+
+    getRecentlyClosed (max) {
+        chrome.sessions.getRecentlyClosed(
+            {
+                maxResults: Number(max) || 10
+            },
+            function (sessions) {
+                let items = [];
+
+                for (var i in sessions) {
+                    if (session.window && session.window.tabs.length === 1) {
+                        session.tab = session.window.tabs[0];
+                    }
+
+                    items.push({
+                        title   : session.tab ? session.tab.title : session.window.tabs.length + ' Tabs',
+                        session : session.window ? session.window.sessionId : session.tab.sessionId,
+                        img     : session.tab ? `url(chrome://favicon/${session.tab.url})` : null
+                    });
+
+                }
+
+                return items;
+            }
+        );
+    }
+
+    getApps () {
+        let find128Image = function (icons) {
+            for (var i in icons) {
+                if (icons[i].size === 128) {
+                    return icons[i].url;
+                }
+            }
+
+            return '/noicon.png';
+        };
+
+        chrome.management.getAll(function (list) {
+            // Only get active apps (no extensions)
+            list = list.filter(function (a) {
+                return a.enabled && a.type !== 'extension' && a.type !== 'theme' && a.isApp
+            });
+
+            // Sort them alphabetically
+            list.sort(function (a, b) {
+                if (a.name < b.name)      { return -1; }
+                else if (a.name > b.name) { return 1; }
+                else                      { return 0; }
+            });
+
+            let items = [];
+
+            for (var i in list) {
+                items.push({
+                    title : extInf.name,
+                    id    : extInf.id,
+                    img   : find128Image(extInf.icons)
+                });
+            }
+        });
+    }
+
+    getShortcuts () {
+        let shortcuts = [
+            {
+                title: 'Bookmarks',
+                url: 'chrome://bookmarks/'
+            },
+            {
+                title: 'History',
+                url: 'chrome://history/'
+            },
+            {
+                title: 'Downloads',
+                url: 'chrome://downloads/'
+            },
+            {
+                title: 'Extensions',
+                url: 'chrome://extensions/'
+            },
+            {
+                title: 'Settings',
+                url: 'chrome://settings/'
+            }
+        ];
+
+        for (var i in shortcuts) {
+            shortcuts[i].img = `url('chrome://favicon/${shortcutItem.url}')`;
+        }
+
+        return shortcuts;
+    }
+}
+
+export default Chrome;
