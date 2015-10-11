1a5f102d4b56b715b055414e17b828b0dd69cb19
Eugene Cheung <ey.cheung@outlook.com>
2015-10-11 15:40:52 -0400

    Toggle panels

--- app/scripts/components/colour/panels.js
+++ app/scripts/components/colour/panels.js
@@ -8,6 +8,7 @@ class Panels extends React.Component {
         super(props);
 
         this.state = {
+            open           : 0,
             topSites       : [],
             recentlyClosed : [],
             apps           : [],
@@ -41,6 +42,14 @@ class Panels extends React.Component {
         });
     }
 
+    onClickToggle (id) {
+        return () => {
+            this.setState({
+                open: this.state.open === id ? 0 : id
+            });
+        };
+    }
+
     onClickSession (session) {
         return function () {
             chrome.sessions.restore(session, null);
@@ -62,76 +71,85 @@ class Panels extends React.Component {
     render () {
         return (
             <div className='panels'>
-                <p id='panel-toggles'>
-                    <a id='panel-toggle-visited'>Most visited</a>
-                    <a id='panel-toggle-closed'>Recently closed</a>
-                    <a id='panel-toggle-apps'>Apps</a>
-                    <a id='panel-toggle-shortcuts'>Shortcuts</a>
+                <p className='panels__toggles'>
+                    <a onClick={this.onClickToggle(1)}>Most visited</a>
+                    <a onClick={this.onClickToggle(2)}>Recently closed</a>
+                    <a onClick={this.onClickToggle(3)}>Apps</a>
+                    <a onClick={this.onClickToggle(4)}>Shortcuts</a>
                 </p>
 
-                <div id='panel'>
-                    <ul id='visited'>
-                        { this.state.topSites && this.state.topSites.map((site, i) => {
-                            let siteStyle = {
-                                backgroundImage: `url('${site.img}')`
-                            };
+                <div className='panels__panel'>
+                    { this.state.open === 1 &&
+                        <ul>
+                            { this.state.topSites.map((site, i) => {
+                                let siteStyle = {
+                                    backgroundImage: `url('${site.img}')`
+                                };
 
-                            return (
-                                <li key={i}>
-                                    <a className={`item-${i}`} style={siteStyle} title={site.title} href={site.url}>
-                                        {site.title}
-                                    </a>
-                                </li>
-                            );
-                        }) }
-                    </ul>
+                                return (
+                                    <li key={i}>
+                                        <a className={`item-${i}`} style={siteStyle} title={site.title} href={site.url}>
+                                            {site.title}
+                                        </a>
+                                    </li>
+                                );
+                            }) }
+                        </ul>
+                    }
 
-                    <ul id='closed'>
-                        { this.state.recentlyClosed && this.state.recentlyClosed.map((session, i) => {
-                            let sessionStyle = {
-                                backgroundImage: `url('${session.img}')`
-                            };
+                    { this.state.open === 2 &&
+                        <ul>
+                            { this.state.recentlyClosed.map((session, i) => {
+                                let sessionStyle = {
+                                    backgroundImage: `url('${session.img}')`
+                                };
 
-                            return (
-                                <li key={i} onClick={this.onClickSession(session.session)}>
-                                    <a className={`item-${i}`} style={sessionStyle} title={session.title}>
-                                        {session.title}
-                                    </a>
-                                </li>
-                            );
-                        }) }
-                    </ul>
+                                return (
+                                    <li key={i} onClick={this.onClickSession(session.session)}>
+                                        <a className={`item-${i}`} style={sessionStyle} title={session.title}>
+                                            {session.title}
+                                        </a>
+                                    </li>
+                                );
+                            }) }
+                        </ul>
+                    }
 
-                    <ul id='apps'>
-                        { this.state.apps && this.state.apps.map((app, i) => {
-                            return (
-                                <li key={i} onClick={this.onClickApp(app.id)}>
-                                    <a className={`item-${i}`}>
-                                        <img src={app.img} />
-                                        <div className='app-name'>{app.title}</div>
-                                    </a>
-                                </li>
-                            );
-                        }) }
 
-                        <a id='store-link' href='https://chrome.google.com/webstore'>Chrome Web Store</a>
-                    </ul>
+                    { this.state.open === 3 &&
+                        <ul>
+                            { this.state.apps.map((app, i) => {
+                                return (
+                                    <li key={i} onClick={this.onClickApp(app.id)}>
+                                        <a className={`panels__app item-${i}`}>
+                                            <img src={app.img} alt={app.title} />
+                                            <div className='app-name'>{app.title}</div>
+                                        </a>
+                                    </li>
+                                );
+                            }) }
 
-                    <ul id='shortcuts'>
-                        { this.state.shortcuts.map((shortcut, i) => {
-                            let shortcutStyle = {
-                                backgroundImage: `url('${shortcut.img}')`
-                            };
+                            <a id='store-link' href='https://chrome.google.com/webstore'>Chrome Web Store</a>
+                        </ul>
+                    }
 
-                            return (
-                                <li key={i} onClick={this.onClickShortcut(shortcut.url)}>
-                                    <a className={`item-${i}`} style={shortcutStyle} title={shortcut.title}>
-                                        {shortcut.title}
-                                    </a>
-                                </li>
-                            );
-                        }) }
-                    </ul>
+                    { this.state.open === 4 &&
+                        <ul>
+                            { this.state.shortcuts.map((shortcut, i) => {
+                                let shortcutStyle = {
+                                    backgroundImage: `url('${shortcut.img}')`
+                                };
+
+                                return (
+                                    <li key={i} onClick={this.onClickShortcut(shortcut.url)}>
+                                        <a className={`item-${i}`} style={shortcutStyle} title={shortcut.title}>
+                                            {shortcut.title}
+                                        </a>
+                                    </li>
+                                );
+                            }) }
+                        </ul>
+                    }
                 </div>
             </div>
         );
