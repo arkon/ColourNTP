9f22f477804d9f3abef3fa742381e361dce5b46a
Eugene Cheung <ey.cheung@outlook.com>
2015-11-08 15:55:42 -0500

    Hide image button when no bg image

--- app/scripts/components/colour/newtab.js
+++ app/scripts/components/colour/newtab.js
@@ -177,7 +177,7 @@ class NewTab extends React.Component {
                     <a target='_blank' className='colours__opts__opt colours__opts__opt--options'
                         href='options.html' title='Options' />
 
-                    { settings && settings.bg &&
+                    { settings && settings.bg !== 'none' &&
                         <a target='_blank' className='colours__opts__opt colours__opts__opt--download'
                             href={this.state.bgImage} title='Open image' />
                     }
