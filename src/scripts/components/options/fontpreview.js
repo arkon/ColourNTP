fc5f548e4f216eba660221d0f7f933f37c27667e
Eugene Cheung <ey.cheung@outlook.com>
2015-12-25 11:38:29 -0500

    Font preview

--- src/scripts/components/options/fontpreview.js
+++ src/scripts/components/options/fontpreview.js
@@ -4,12 +4,43 @@ import Chrome from '../../modules/chrome';
 import WebFont from '../../modules/webfont';
 
 
-var FontPreview = (props) => {
-    WebFont.loadFont(props.font);
+class FontPreview extends React.Component {
+    constructor (props) {
+        super(props);
 
-    return (
-        <p>{props.font}</p>
-    );
-};
+        this.state = {
+            font: this.props.font
+        };
+
+        this.fetchSettings = this.fetchSettings.bind(this);
+    }
+
+    componentDidMount () {
+        WebFont.loadFont(this.state.font);
+
+        // Fetch new settings when changed
+        chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+            if (request.msg === 'saved') {
+                this.fetchSettings();
+            }
+        });
+    }
+
+    fetchSettings () {
+        Chrome.getSettings((settings) => {
+            WebFont.loadFont(settings.fontWeb);
+
+            this.setState({
+                font: settings.fontWeb
+            });
+        });
+    }
+
+    render () {
+        return (
+            <p>Preview: <span style={{ fontFamily: this.state.font }}>12:34:56</span></p>
+        );
+    }
+}
 
 export default FontPreview;
