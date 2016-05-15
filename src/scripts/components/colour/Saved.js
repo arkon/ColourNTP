0a8b1f07ad1715a91d60f3afcb0c28ef501a2f68
Eugene Cheung <ey.cheung@outlook.com>
2016-05-15 10:48:36 -0400

    Add placeholder for Saved component

---
+++ src/scripts/components/colour/Saved.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import Chrome from '../../modules/chrome';
+
+class Saved extends React.Component {
+  constructor (props) {
+    super(props);
+  }
+
+  render () {
+    return null;
+  }
+}
+
+export default Saved;
