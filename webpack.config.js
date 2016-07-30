3a78b7f36bc7a0719fbf48c3fcaf6b72be035657
Eugene Cheung <ey.cheung@outlook.com>
2016-07-30 10:48:52 -0400

    Configure common chunks properly

--- webpack.config.js
+++ webpack.config.js
@@ -36,23 +36,11 @@ module.exports = {
     ]
   },
   plugins: [
-    new webpack.optimize.CommonsChunkPlugin({
-      name: 'vendor',
-      minChunks: Infinity,
-      filename: 'vendor.js'
-    }),
+    new webpack.optimize.DedupePlugin(),
+    new webpack.optimize.CommonsChunkPlugin('vendor'),
     new webpack.LoaderOptionsPlugin({
       minimize: true,
       debug: false
-    }),
-    // new webpack.optimize.UglifyJsPlugin({
-    //   compress: {
-    //     warnings: false
-    //   },
-    //   output: {
-    //     comments: false
-    //   },
-    //   sourceMap: false
-    // })
+    })
   ]
 };
