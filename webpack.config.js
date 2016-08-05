71f444f3d98a1b9a7218ac0074dc1e6c00ef8d70
Eugene Cheung <ey.cheung@outlook.com>
2016-08-05 18:41:24 -0400

    Updates

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,6 @@
 const webpack = require('webpack');
 const path = require('path');
 
-const nodeEnv = process.env.NODE_ENV || 'development';
-const isProd = nodeEnv === 'production';
-
 module.exports = {
   context: path.join(__dirname, './src/scripts'),
   entry: {
